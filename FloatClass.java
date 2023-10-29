db163dc4858adbba61d7bcd29bf6b0cd60dd5fc1
Neha Thaker <thakersmail@yahoo.co.uk>
2023-10-29 22:05:20 +0000

    mythirdcommit

---
+++ FloatClass.java
@@ -0,0 +1,17 @@
+package datatypefloatpackage;
+
+public class FloatClass {
+
+    public static void main ( String [] args ) {
+
+        float a = 20;
+        float b = 40;
+
+        System.out.println( a + b );
+        System.out.println( a - b );
+        System.out.println( a * b );
+        System.out.println( a / b );
+        System.out.println( a % b );
+
+    }
+}
